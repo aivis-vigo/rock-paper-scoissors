f048a0c74005bdd75e3d32ea1fc30a5d1a0f92fe
aivis-vigo <aivisvigoreimarts@gmail.com>
2023-07-16 11:46:17 +0300

    Update

--- app/Game.php
+++ app/Game.php
@@ -1,12 +1,16 @@
 <?php declare(strict_types=1);
 
+namespace App;
+
+use JetBrains\PhpStorm\NoReturn;
+
 class Game
 {
     private int $you;
     private int $opponentWins;
     private int $round;
     private int $number;
-    private array $opponent;
+    private array $opponents;
     private array $elements;
     private array $winningElements;
 
@@ -17,7 +21,7 @@ class Game
         $this->number = 0;
         $this->round = 1;
 
-        $this->opponent =  [
+        $this->opponents =  [
             'Snickerdoodle McFizz',
             'Noodle Noodleman',
             'Bumble Bopkins'
@@ -40,52 +44,52 @@ class Game
         ];
     }
 
-    public function run(): void
+    #[NoReturn] public function run(): void
     {
         echo "====================================" . PHP_EOL;
         echo "Rock, Paper, Scissors, Lizard, Spock" . PHP_EOL;
         echo "====================================" . PHP_EOL;
 
-        $opponentName = $this->opponent[$this->number];
+        $opponentName = $this->selectOpponent();
 
         while(true) {
-            $computerChoice = $this->elements[2];//[array_rand($this->elements)];
+            $opponentChoice = $this->opponentThrows();
 
             echo PHP_EOL;
             echo "You: $this->you vs $opponentName: $this->opponentWins" . PHP_EOL;
             echo PHP_EOL;
 
-            $playerChoice = strtolower(readline('Your Choice: '));
+            $playerChoice = $this->playerInput();
 
             while (!array_key_exists($playerChoice, $this->winningElements)) {
                 echo PHP_EOL;
                 echo "Element '$playerChoice' doesn't exist!" . PHP_EOL;
-                $playerChoice = strtolower(readline('Your Choice: '));
+                $playerChoice = $this->playerInput();
             }
 
-            $message = "You: " . ucfirst($playerChoice) . " vs $opponentName: " . ucfirst($computerChoice);
+            $message = "You: " . ucfirst($playerChoice) . " vs $opponentName: " . ucfirst($opponentChoice);
 
-            if ($computerChoice == $playerChoice)
+            if ($opponentChoice == $playerChoice)
             {
                 echo "$message. Draw!" . PHP_EOL;
                 echo "------------------------------------" . PHP_EOL;
                 echo PHP_EOL;
-            } elseif (in_array($computerChoice, $this->winningElements[$playerChoice])) {
+            } elseif (in_array($opponentChoice, $this->winningElements[$playerChoice])) {
                 echo "$message. You Won!" . PHP_EOL;
                 echo PHP_EOL;
                 echo "------------------------------------" . PHP_EOL;
 
-                $this->you++;
+                $this->playerWins();
             } else {
                 echo "$message. PC Won!" . PHP_EOL;
                 echo PHP_EOL;
                 echo "------------------------------------" . PHP_EOL;
 
-                $this->opponentWins++;
+                $this->opponentWins();
             }
 
             if ($this->you == 3 || $this->opponentWins == 3) {
-                $winner = ($this->you == 3) ? "You" : $opponentName;
+                $winner = $this->determineWinner($opponentName);
 
                 echo "You: $this->you vs $opponentName: $this->opponentWins" . PHP_EOL;
 
@@ -99,9 +103,9 @@ class Game
                         echo PHP_EOL;
                         echo "------------------------------------" . PHP_EOL;
                         echo "You won round $this->round!" . PHP_EOL;
-                        $this->round++;
+                        $this->nextRound();
 
-                        if ($this->round > count($this->opponent)) {
+                        if ($this->round > count($this->opponents)) {
                             echo "You are Rock-Paper-Scissors-Lizard-Spock Champion!" . PHP_EOL;
                             echo "====================================" . PHP_EOL;
                             exit;
@@ -111,12 +115,57 @@ class Game
                         echo "Round $this->round" . PHP_EOL;
                         echo "====================================" . PHP_EOL;
 
-                        $this->you = 0;
-                        $this->opponentWins = 0;
-                        $this->number += 1;
-                        $opponentName = $this->opponent[$this->number];
+                        $this->resetScores();
+                        $opponentName = $this->nextOpponent();
                 }
             }
         }
+    }
+
+    private function selectOpponent(): string
+    {
+        return $this->opponents[$this->number];
+    }
+
+    private function opponentThrows(): string
+    {
+        return $this->elements[array_rand($this->elements)];
+    }
+
+    private function playerInput(): string
+    {
+        return strtolower(readline('Your Choice: '));
+    }
+
+    private function playerWins(): void
+    {
+        $this->you++;
+    }
+
+    private function opponentWins(): void
+    {
+        $this->opponentWins++;
+    }
+
+    private function determineWinner(string $opponent): string
+    {
+        return ($this->you == 3) ? "You" : $opponent;
+    }
+
+    private function nextRound(): void
+    {
+        $this->round++;
+    }
+
+    private function resetScores(): void
+    {
+        $this->you = 0;
+        $this->opponentWins = 0;
+    }
+
+    private function nextOpponent(): string
+    {
+        $this->number += 1;
+        return $this->opponents[$this->number];
     }
 }
